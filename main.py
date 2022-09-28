e84beffa059bba36dc940f2270665252abc0b414
Lorenzo Hernandez-Nunez <113930435+Lorenzo1135@users.noreply.github.com>
2022-09-28 17:56:38 +0000

    decision making

--- main.py
+++ main.py
@@ -140,8 +140,10 @@ num2=5
 # f"{num1} is greater than {num2}"
 # "num2 is greater than num1"
 # "num1 and num2 are equal"
-
-
+pet= 'rabbit'
+if pet== 'cat':
+ print("you have a cat")
+elif pet== 'dog': 
 # Decision Making Practice #2
 # The laws of a certain country establish that an adult can drive if they are of legal age (18 years or older), and have a driver's license.
 
